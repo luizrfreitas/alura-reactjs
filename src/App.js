bf4ed1c861d25373722ae1af3df7d171a368aad5
Luiz R Freitas <luizfreitasaca@gmail.com>
2022-08-22 20:59:13 -0300

    Conclusão

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React,{ Component } from "react";
-import { FormularioCadastro } from "./components/FormularioCadastro.jsx";
-import { ListaDeNotas } from "./components/ListaDeNotas.jsx";
-
-// Isso é um componente.
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
+  }
+
   render() {
     return (
-      <section>
-        <FormularioCadastro/>
-        <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
 }
 
 export default App;
-
-// Para inciar um app é precisa chamar no terminal por:
-// npm start
-//jsx ->
-//acent JSX elements must be wrapped in an enclosing tag. Did you want a JSX fragment <>...</>?
-// Isso significa que não pode ter dois elementos irmãos
